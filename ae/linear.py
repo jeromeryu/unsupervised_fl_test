70eb7af599810052518eb0190fa96fdcbad94e2b
major.ryu <54859212+jeromeryu@users.noreply.github.com>
2021-07-26 23:13:35 +0900

    fix: bug

--- ae/linear.py
+++ ae/linear.py
@@ -23,7 +23,7 @@ class Net(nn.Module):
         # classifier
         self.fc = nn.Linear(2048, num_class, bias=True)
         # self.load_state_dict(torch.load(pretrained_path, map_location='cpu'), strict=False)
-        self.load_state_dict(net, strict=False)
+        # self.load_state_dict(net, strict=False)
 
     def forward(self, x):
         x = self.f(x)
