175c1a0fbef2dbe83397e1894e990734ebdf4f09
major.ryu <54859212+jeromeryu@users.noreply.github.com>
2021-07-27 21:52:23 +0900

    fix: bug

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ import torch.nn.functional as F
 import torch.optim as optim
 import matplotlib.pyplot as plt
 from torch.utils.data.sampler import SubsetRandomSampler
+import pandas as pd
 
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
 from cae.cae import ConvAutoencoder, ContrastiveLoss
 import ae.utils
+import cae.linear 
+import os
 
 device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
 
@@ -52,7 +55,45 @@ for epoch in range(num_epochs):
     running_loss = np.mean(losses)
     print(f'Epoch {epoch}: {running_loss}')
     if epoch % 10 == 9:
-        ae.utils.display_output(data, out, 32, 32, '%d ori.png'.format(epoch/10), '%d recon.png'.format(epoch/10))
+        ae.utils.display_output(data, out, 32, 32, '{} ori.png'.format(epoch/10), '{} recon.png'.format(epoch/10))
 
 
+
+train_data_linear = datasets.CIFAR10(root='data', train=True,
+                                   download=True, transform=transform)
+test_data_linear = datasets.CIFAR10(root='data', train=False,
+                                  download=True, transform=transform)
+
+train_loader_linear = DataLoader(train_data, batch_size=batch_size, num_workers=num_workers)
+test_loader_linear = DataLoader(test_data, batch_size=batch_size, num_workers=num_workers)
+
+linear_epoch = 100
+net = cae.linear.Net(num_class=len(train_loader_linear.classes), net = model).cuda()
+for param in net.f.parameters():
+    param.requires_grad = False
+optimizer = optim.Adam(net.fc.parameters(), lr=1e-3, weight_decay=1e-6)
+
+
+results = {'train_loss': [], 'train_acc@1': [], 'train_acc@5': [],
+            'test_loss': [], 'test_acc@1': [], 'test_acc@5': []}
+
+if not os.path.exists('results'):
+    os.mkdir('results')
+
+for epoch in range(1, linear_epoch + 1):
+    train_loss, train_acc_1, train_acc_5 = cae.linear.train_val(net, train_loader_linear, optimizer)
+    results['train_loss'].append(train_loss)
+    results['train_acc@1'].append(train_acc_1)
+    results['train_acc@5'].append(train_acc_5)
+    test_loss, test_acc_1, test_acc_5 = cae.linear.train_val(net, test_loader_linear, None)
+    results['test_loss'].append(test_loss)
+    results['test_acc@1'].append(test_acc_1)
+    results['test_acc@5'].append(test_acc_5)
+    print(test_loss, test_acc_1, test_acc_5)
+    # save statistics
+    data_frame = pd.DataFrame(data=results, index=range(1, epoch + 1))
+    data_frame.to_csv('results/cae_linear_statistics.csv', index_label='epoch')
+    # if test_acc_1 > best_acc:
+    #     best_acc = test_acc_1
+    #     torch.save(model.state_dict(), 'results/linear_model.pth')
 
