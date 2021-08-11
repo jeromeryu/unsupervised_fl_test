c6b8d51a0032218f85ccba7f729d56ee46ae98c9
major.ryu <54859212+jeromeryu@users.noreply.github.com>
2021-08-11 18:54:49 +0900

    fix: loss

--- load_data.py
+++ load_data.py
@@ -1,11 +1,13 @@
+from tarfile import NUL
 import torch
+from torch.utils import data
 from torch.utils.data import DataLoader, Dataset
 
 class DatasetSplit(Dataset):
     """An abstract Dataset class wrapped around Pytorch Dataset class.
     """
 
-    def __init__(self, dataset, idxs):
+    def __init__(self, dataset=None, idxs=None):
         self.dataset = dataset
         self.idxs = [int(i) for i in idxs]
 
@@ -21,7 +23,9 @@ if __name__=='__main__':
     num_split = 10
     save_path = 'data_test/'
     for i in range(num_split):
+        dataset = DatasetSplit()
         dataset = torch.load(save_path + str(i))
+        dataset = dataset.load_state_dict()
         print(len(dataset))
         dataloader = DataLoader(dataset, batch_size = 128)
         for j in dataloader:
