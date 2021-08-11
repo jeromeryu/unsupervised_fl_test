f436dabfbe7762e336d9b308fa63189018dd202c
major.ryu <54859212+jeromeryu@users.noreply.github.com>
2021-08-12 00:36:44 +0900

    fix: loss

--- split_dataset.py
+++ split_dataset.py
@@ -2,6 +2,7 @@ import numpy as np
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, Dataset
 import torch
+import json
 
 class DatasetSplit(Dataset):
     """An abstract Dataset class wrapped around Pytorch Dataset class.
@@ -37,16 +38,22 @@ def cifar_iid(dataset, num_users):
 
 
 if __name__=='__main__':
-    data_path = 'data'
+    data_path = 'data_test'
     num_split = 10
-    save_path = 'data_test/'
+    save_path = 'data_test/cifar_10_split.json'
 
     train_data = datasets.CIFAR10(root=data_path, train=True, download=True)
     user_groups = cifar_iid(train_data, num_split)
     
+    json_obj = dict()
     for i in range(num_split):
-        dataset = DatasetSplit(train_data, user_groups[i])
+        # dataset = DatasetSplit(train_data, user_groups[i])
         # trainloader = DataLoader(dataset, batch_size=128, )
-        torch.save(dataset, save_path + str(i))
+        # torch.save(dataset, save_path + str(i))
+        json_obj[i] = user_groups[i]
+    
+    
+    with open(save_path, 'w') as json_file:
+        json.dump(json_obj, json_file)
     
     
